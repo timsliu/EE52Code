d9611786535130c1236f6ebe8e42036e4e8b3128
TLiuCaltech <Timothy@MacBook-Pro-3.local>
2016-04-04 21:14:41 -0700

    4/4/16

--- MIRQ.asm
+++ MIRQ.asm
@@ -1,8 +1,202 @@
     NAME    MIRQ
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;                                                                            ;
-;                                    Switch                                  ;
-;                               Switch Functions                             ;
+;                                MP3 Interrupts                              ;
+;                           MP3 Interrupt Functions                          ;
 ;                                   EE/CS 51                                 ;
 ;                                                                            ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Description:  This files contains the functions relating to interrupts
+;               for the MP3 player. The functions clear the interrupt
+;               vector table, installs the hardware and timer interrupts,
+;               and installs a handler for illegal interrupts.
+;
+
+; Table of Contents
+;
+;    ClrIRQVectors          -clear the interrupt vector table
+;    IllegalEventHandler    -takes care of illegal events
+;    InstallDreqHandler     -installs VS1011 data request IRQ handler
+;    InstallDemandHandler   -installs CON_MP3 data demand IRQ handler
+;    InstallTimer0Handler   -installs the timer0 handler
+
+
+Revisioin History:
+;    4/4/16
+
+$INCLUDE(MIRQ.INC)
+
+CGROUP    GROUP    CODE
+
+CODE SEGMENT  PUBLIC ‘CODE’
+
+        ASSUME    CS:GROUP
+
+; external function declarations
+
+    ;EXTRN    DreqEH             ;VS1011 data request IRQ handler
+    ;EXTRN    DemandEH           ;CON_MP3 data demand handler
+    ;EXTRN    ButtonEH           ;checks if a button is pressed
+
+; ClrIRQVectors
+;
+; Description:      This functions installs the IllegalEventHandler for all
+;                   interrupt vectors in the interrupt vector table.  Note
+;                   that all 256 vectors are initialized so the code must be
+;                   located above 400H.  The initialization skips  (does not
+;                   initialize vectors) from vectors FIRST_RESERVED_VEC to
+;                   LAST_RESERVED_VEC.
+;
+; Arguments:        None.
+; Return Value:     None.
+;
+; Local Variables:  CX    - vector counter.
+;                   ES:SI - pointer to vector table.
+; Shared Variables: None.
+; Global Variables: None.
+;
+; Input:            None.
+; Output:           None.
+;
+; Error Handling:   None.
+;
+; Algorithms:       None.
+; Data Structures:  None.
+;
+; Registers Used:   flags, AX, CX, SI, ES
+; Stack Depth:      0 word
+;
+; Author:           Timothy Liu
+; Last Modified:    10/27/15
+
+ClrIRQVectors   PROC    NEAR
+                PUBLIC  ClrIRQVectors
+
+
+InitClrVectorLoop:              ;setup to store the same handler 256 times
+
+        XOR     AX, AX          ;clear ES (interrupt vectors are in segment 0)
+        MOV     ES, AX
+        MOV     SI, 0           ;initialize SI to the first vector
+
+        MOV     CX, NUM_IRQ_VECTORS      ;maximum number to initialize
+
+
+ClrVectorLoop:                  ;loop clearing each vector
+                                ;check if should store the vector
+        CMP     SI, INTERRUPT_SIZE * FIRST_RESERVED_VEC
+        JB      DoStore         ;if before start of reserved field - store it
+        CMP     SI, INTERRUPT_SIZE * LAST_RESERVED_VEC
+        JBE     DoneStore       ;if in the reserved vectors - don't store it
+        ;JA     DoStore         ;otherwise past them - so do the store
+
+DoStore:                        ;store the vector
+        MOV     ES: WORD PTR [SI], OFFSET(IllegalEventHandler)
+        MOV     ES: WORD PTR [SI + OffSize], SEG(IllegalEventHandler)
+
+DoneStore:                      ;done storing the vector
+        ADD     SI, INTERRUPT_SIZE           ;update pointer to next vector
+
+        LOOP    ClrVectorLoop   ;loop until have cleared all vectors
+        ;JMP    EndClrIRQVectors;and all done
+
+
+EndClrIRQVectors:               ;all done, return
+        RET
+
+
+ClrIRQVectors   ENDP
+
+
+
+; IllegalEventHandler
+;
+; Description:       This procedure is the event handler for illegal
+;                    (uninitialized) interrupts.  It does nothing - it just
+;                    returns after sending a non-specific EOI.
+;
+; Operation:         Send a non-specific EOI and return.
+;
+; Arguments:         None.
+; Return Value:      None.
+;
+; Local Variables:   None.
+; Shared Variables:  None.
+; Global Variables:  None.
+;
+; Input:             None.
+; Output:            None.
+;
+; Error Handling:    None.
+;
+; Algorithms:        None.
+; Data Structures:   None.
+;
+; Registers Changed: None
+; Stack Depth:       2 words
+;
+; Author:            Timothy Liu
+; Last Modified:     10/27/15
+
+IllegalEventHandler     PROC    NEAR
+                        PUBLIC  IllegalEventHandler
+
+        NOP                             ;do nothing (can set breakpoint here)
+
+        PUSH    AX                      ;save the registers
+        PUSH    DX
+
+        MOV     DX, INTCtrlrEOI         ;send a non-sepecific EOI to the
+        MOV     AX, NonSpecEOI          ;   interrupt controller to clear out
+        OUT     DX, AL                  ;   the interrupt that got us here
+
+        POP     DX                      ;restore the registers
+        POP     AX
+
+        IRET                            ;and return
+
+
+IllegalEventHandler     ENDP
+
+; InstallDreqHandler
+;
+; Description:       This function installs the event handler for the data
+;                    request interrupt from the VS1011 MP3 decoder. The
+;                    function
+;
+; Operation:         None
+;
+; Arguments:         None.
+; Return Value:      None.
+;
+; Local Variables:   None.
+; Shared Variables:  None.
+; Global Variables:  None.
+;
+; Input:             None.
+; Output:            None.
+;
+; Error Handling:    None.
+;
+; Algorithms:        None.
+; Data Structures:   None.
+;
+; Registers Changed: None
+;
+; Author:            Timothy Liu
+; Last Modified:     4/4/16
+
+InstallDreqHandler    PROC    NEAR
+                      PUBLIC  InstallDreqHandler
+
+##### InstallDreqHandler CODE #######
+
+
+InstallDreqHandler    ENDP
+    
+
+
+CODE ENDS
+
+END
--- button.asm
+++ button.asm
@@ -1,9 +1,9 @@
-    NAME    SWITCH
+    NAME    BUTTON
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;                                                                            ;
-;                                    Switch                                  ;
-;                               Switch Functions                             ;
+;                                    Button                                  ;
+;                               Button Functions                             ;
 ;                                   EE/CS 51                                 ;
 ;                                                                            ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -13,24 +13,25 @@
 ; Revision History:
 ;        2/3/16    Tim Liu    created file
 ;        2/4/16    Tim Liu    finished writing outline
+;        4/4/16    Tim Liu    changed button to buttons
 ;
 ;
 ; Table of Contents
 ;
-;        InitSwitches - initializes the functions needed for switches
-;        SwitchDebounce - scans a the key address and debounces
+;        InitButtons - initializes the functions needed for buttons
+;        ButtonDebounce - scans a the key address and debounces
 ;        key_available - returns TRUE if there is a valid key
 ;        getkey - returns the key code for the debounced key
 
-;Name:               InitSwitches
+;Name:               InitButtons
 ;
 ;Description:        This function initializes the shared variables for the
-;                    switch functions. The function sets the value of
+;                    button functions. The function sets the value of
 ;                    DebounceCnt to zero and sets LastRead to NoKeyPressed.
 ;
 ;Operation:          The function sets each of the three shared variables
 ;                    to the listed values. DebounceCnt is set to zero and
-;                    LastRead is set to NoSwitchPressed.
+;                    LastRead is set to NobuttonPressed.
 ;
 ;Arguments:          None
 ;
@@ -58,9 +59,9 @@
 ;Last Modified:      2/4/16
 
 ;Outline
-;InitSwitches()
+;InitButtons()
 ;    DebounceCnt = 0                     ;clear the debounce timer
-;    LastRead = NoSwitchPressed          ;indicate no key is pressed
+;    LastRead = NoButtonPressed          ;indicate no key is pressed
 ;    RETURN
 
 
@@ -70,30 +71,30 @@
 
 
 
-;Name:               SwitchDebounce()
+;Name:               ButtonDebounce()
 ;
-;Description:        This function reads the address of the switches to
-;                    see if a switch is pressed. If a switch is pressed,
-;                    the function debounces the press and enqueues the switch
-;                    code to the SwitchQueue using the function EnqueueEvent.
+;Description:        This function reads the address of the buttons to
+;                    see if a button is pressed. If a button is pressed,
+;                    the function debounces the press and enqueues the button
+;                    code to the buttonQueue using the function EnqueueEvent.
 ;                    This function also implements auto repeat. If the 
-;                    same switch is held down, then the switch press will
+;                    same button is held down, then the button press will
 ;                    be recorded every RepeatRate milliseconds. Only one
 ;                    button can be pushed at a time. If multiple buttons
 ;                    are pressed, the function will act like no
 ;                    buttons are being pressed. This function is called by
-;                    the interrupt handler SwitchHandler every millisecond.
+;                    the interrupt handler buttonHandler every millisecond.
 ;
 ;Operation:          When called, the function reads in from the address
-;                    SwitchAddress. If the value of the input is equal to
-;                    NoSwitchPressed or if the value of the input is not
+;                    buttonAddress. If the value of the input is equal to
+;                    NoButtonPressed or if the value of the input is not
 ;                    equal to LastRead, then the function resets the debounce
 ;                    counter. If the value of the input is different from
 ;                    the last input, then LastRead is updated. If the input
 ;                    is the same as LastRead, then the function decrements
 ;                    DebounceCnt. Once DebounceCnt reaches zero, the function
 ;                    calls EnqueueEvent and enqueues the key press to the
-;                    SwitchQueue.
+;                    buttonQueue.
 ;
 ;Arguments:          None
 ;
@@ -106,7 +107,7 @@
 ;                    LastRead (R/W) - the value of the last key that was
 ;                    pressed.
 ;
-;Input:              8 possible switch presses from 8 different switches
+;Input:              8 possible button presses from 8 different buttons
 ;
 ;Output:             None
 ;
@@ -124,25 +125,25 @@
 ;
 ;Outline
 ;
-;SwitchDebounce()
+;buttonDebounce()
 
-;Input(ReadAddress, SwitchInput)          ;read a word from the port
+;Input(ReadAddress, buttonInput)          ;read a word from the port
 ;
-;IF (SwitchInput == NoKeyPressed) OR      ;check if switch was pressed
-;    (SwitchInput != LastRead):           ;check if switch is same
+;IF (ButtonInput == NoKeyPressed) OR      ;check if button was pressed
+;    (ButtonInput != LastRead):           ;check if button is same
 ;    DebounceCnt = DebounceTime           ;reset the debounce counter
-;    LastRead = KeyInput                  ;update which switch was pressed
+;    LastRead = KeyInput                  ;update which button was pressed
 ;
-;ELSE:                                    ;if a switch was pressed
-;    DebounceCnt --                       ;debounce the switch
-;    IF DebounceCnt == 0:                 ;switch is debounced
-;        CALL EnQueue                     ;enqueue the switch press event
+;ELSE:                                    ;if a button was pressed
+;    DebounceCnt --                       ;debounce the button
+;    IF DebounceCnt == 0:                 ;button is debounced
+;        CALL EnQueue                     ;enqueue the button press event
 ;        DebounceCnt = RepeatRate         ;implement auto-repeat
 ;
 ;RETURN
 
 
-; ###### SwitchDebounce CODE  ######
+; ###### ButtonDebounce CODE  ######
 
 
 
@@ -150,21 +151,21 @@
 
 ;Name:               Key_Available
 ;
-;Description:        This function checks if there is a switch press ready
+;Description:        This function checks if there is a button press ready
 ;                    for processing. The function calls QueueEmpty to check
-;                    if the SwitchQueue is empty. If SwitchQueue is empty,
+;                    if the buttonQueue is empty. If buttonQueue is empty,
 ;                    then the function returns TRUE and if there is no
-;                    switch press ready the function returns FALSE.
+;                    button press ready the function returns FALSE.
 ;
-;Operation:          Call QueueEmpty to check if the SwitchQueue has any
+;Operation:          Call QueueEmpty to check if the buttonQueue has any
 ;                    elements in it. If QueueEmpty returns with the 
-;                    SwitchQueue empty, the function returns with FALSE in
+;                    buttonQueue empty, the function returns with FALSE in
 ;                    AX. Otherwise, the function returns TRUE in AX.
 ;
 ;Arguments:          None
 ;
-;Return Values:      HaveSwitch (AX) - TRUE if a switch is ready to be
-;                    processed; FALSE if no switch press
+;Return Values:      Havebutton (AX) - TRUE if a button is ready to be
+;                    processed; FALSE if no button press
 ;
 ;Local Variables:    QueueAddress (SI) - address of queue. Argument to
 ;                    QueueEmpty
@@ -187,26 +188,17 @@
 
 ;Outline
 ;Key_Available():
-;    QueueAddress = Address(SwitchQueue)        ;load the argument
+;    QueueAddress = Address(buttonQueue)        ;load the argument
 ;    IF QueueEmpty(QueueAddress) == TRUE:       ;call function to check if
-;                                               ;switch queue is empty
-;        HaveSwitch == FALSE                    ;no switch available
-;    ELSE:                                      ;otherwise, there’s a switch
-;        HaveSwitch == TRUE                     ;indicate a switch is ready
+;                                               ;button queue is empty
+;        HaveButton == FALSE                    ;no button available
+;    ELSE:                                      ;otherwise, there’s a button
+;        HaveButton == TRUE                     ;indicate a button is ready
 ;    RETURN
 
 
 
 END
-
-
-
-
-
-
-
-
-
 
 
 
